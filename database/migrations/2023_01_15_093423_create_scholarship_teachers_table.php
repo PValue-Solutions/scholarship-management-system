3e6b51ecd7e195f7eea0266361b884e0841feda4
Nirjhor <shah.nirjhor@gmail.com>
2023-01-18 14:13:58 +0600

    teacher table create & modal done

--- database/migrations/2023_01_15_093423_create_scholarship_teachers_table.php
+++ database/migrations/2023_01_15_093423_create_scholarship_teachers_table.php
@@ -15,7 +15,22 @@ class CreateScholarshipTeachersTable extends Migration
     {
         Schema::create('scholarship_teachers', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('company_id')->nullable();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->enum('school_or_college',['1','2'])->default('1');
+            $table->bigInteger('scholarship_school_id')->nullable();
+            $table->bigInteger('scholarship_college_id')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('locale')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->enum('gender', ['male', 'female'])->nullable();
+            $table->enum('blood_group', ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'])->nullable();
+            $table->enum('status', ['0', '1'])->default('0');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
